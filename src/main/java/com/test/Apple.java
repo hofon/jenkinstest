53ad02e8c93c21910d02a452e72dfd5c17ac1564
373411396@qq.com <373411396@qq.com>
2019-06-14 15:44:49 +0800

    test

---
+++ src/main/java/com/test/Apple.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Apple {
+    private String appName="ddsss";
+    public String getAppName(){
+        return this.appName;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
+<%@ page import="com.test.Apple" %>
 <html>
 <body>
 <h2>Hello World!</h2>
+<%=new Apple().getAppName()%>
 </body>
 </html>
